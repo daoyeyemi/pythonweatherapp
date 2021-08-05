fdb4760cac60e428032c8bbfb55bb76b06f0acfe
daoyeyemi <67729483+daoyeyemi@users.noreply.github.com>
2021-08-05 03:03:41 -0700

    was able to extract necessary data from api

--- weather/views.py
+++ weather/views.py
@@ -11,4 +11,6 @@ def display_weather(request):
 def get_weather_info(request):
     # making text readable
     response = requests.get(f"http://api.openweathermap.org/data/2.5/weather?q=London&appid=b55cf10b447457a324c51468a78d2076").json()
-    return render(request, 'weather.html', {'response': response})
+    return render(request, 'index.html', {'response': response})
+
+# def print_search_bar(request):
