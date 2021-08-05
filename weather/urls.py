c72d3b1b4afd8adfaf861b3fc4821a7a236a2fc2
daoyeyemi <67729483+daoyeyemi@users.noreply.github.com>
2021-08-05 02:13:59 -0700

    got weather info for london to show up

--- weather/urls.py
+++ weather/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('climate/', views.display_weather, name='display_weather')
+    path('climate/', views.display_weather, name='display_weather'),
+    path('city/', views.get_weather_info, name='get_weather_info')
 ]
--- weather/views.py
+++ weather/views.py
@@ -1,8 +1,14 @@
 from django.http.response import HttpResponse
 from django.shortcuts import render
+import requests
 
 def index(request):
     return HttpResponse('Yoooooo')
 
 def display_weather(request):
     return render(request, 'index.html')
+
+def get_weather_info(request):
+    # making text readable
+    response = requests.get(f"http://api.openweathermap.org/data/2.5/weather?q=London&appid=b55cf10b447457a324c51468a78d2076").json()
+    return render(request, 'weather.html', {'response': response})
