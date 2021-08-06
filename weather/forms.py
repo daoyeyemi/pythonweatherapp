9ccc39fd6552fc5a1e6bead2bf902607efea4fda
daoyeyemi <67729483+daoyeyemi@users.noreply.github.com>
2021-08-06 15:48:05 -0700

    added a form linked to a model

--- weather/forms.py
+++ weather/forms.py
@@ -1,2 +1,8 @@
-from django import forms
+from django.forms import ModelForm, TextInput
+from weather.models import City
 
+class CityForm(ModelForm):
+    class Meta:
+        model = City
+        fields = ['city_name']
+        widgets = {'city_name' : TextInput(attrs={'class': 'input', 'placeholder' : 'Type city name here...'})}
--- weather/models.py
+++ weather/models.py
@@ -1,4 +1,4 @@
 from django.db import models
 
 class City(models.Model):
-    city_name = models.CharField(max_length=230)
+    city_name = models.CharField(max_length=50)
