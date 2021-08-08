4666b391e1b8a4ab9b625d286f28ac106e2f52b1
daoyeyemi <67729483+daoyeyemi@users.noreply.github.com>
2021-08-07 21:41:09 -0700

    got rid of unnecessary code - commented it out

--- weather/views.py
+++ weather/views.py
@@ -13,14 +13,14 @@ def display_weather(request):
 # def get_inputvalue(request):
 
 def get_weather_info(request):
-    form = CityForm(request.POST)
-    if request.method == 'POST':
-            if form.is_valid():
-                form.save()
+    # form = CityForm(request.POST)
+    # if request.method == 'POST':
+    #         if form.is_valid():
+    #             form.save()
 
-    form = CityForm()
-    print(request.POST.get("city_input"))
+    # form = CityForm()
+    # print(request.POST.get("city_input"))
     city = request.POST.get("city_input")
     # making text readable
     response = requests.get("http://api.openweathermap.org/data/2.5/weather?q={}&appid=b55cf10b447457a324c51468a78d2076".format(city)).json()
-    return render(request, 'index.html', { 'response': response, 'form': form  })
+    return render(request, 'index.html', { 'response': response  })
