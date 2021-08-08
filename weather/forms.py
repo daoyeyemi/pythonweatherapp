c771a07a186a9f595f1efcac3a576d06de348ac3
daoyeyemi <67729483+daoyeyemi@users.noreply.github.com>
2021-08-07 21:25:38 -0700

    finally got everything to work in app

--- weather/forms.py
+++ weather/forms.py
@@ -5,4 +5,4 @@ class CityForm(ModelForm):
     class Meta:
         model = City
         fields = ['city_name']
-        widgets = {'city_name' : TextInput(attrs={'class': 'input', 'placeholder' : 'Type city name here...'})}
+        widgets = { 'city_name' : TextInput(attrs={'class': 'input', 'placeholder' : 'Type city name here...', 'name' :'city_input' })}
--- weather/views.py
+++ weather/views.py
@@ -1,6 +1,7 @@
 from django.http.response import HttpResponse
 from django.shortcuts import render
 from django import forms
+from .forms import CityForm
 import requests
 
 def index(request):
@@ -12,14 +13,14 @@ def display_weather(request):
 # def get_inputvalue(request):
 
 def get_weather_info(request):
+    form = CityForm(request.POST)
     if request.method == 'POST':
-        city = 'Lagos'
+            if form.is_valid():
+                form.save()
+
+    form = CityForm()
+    print(request.POST.get("city_input"))
+    city = request.POST.get("city_input")
     # making text readable
     response = requests.get("http://api.openweathermap.org/data/2.5/weather?q={}&appid=b55cf10b447457a324c51468a78d2076".format(city)).json()
-    return render(request, 'index.html', { 'response': response  })
-
-# def print_search_bar(request):
-#     print()
-
-
-    
+    return render(request, 'index.html', { 'response': response, 'form': form  })
